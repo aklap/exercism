d2d0aebcad77ea311c91e229f35aa4918eb16944
Alexis La Porte <aklaporte@gmail.com>
2016-09-15 16:36:34 -0400

    add other tracks

---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,10 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+
+def hello(name=''):
+    if name == '':
+        return 'Hello, World!'
+    elif name:
+        return 'Hello, ' + name + '!' 

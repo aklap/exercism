1a21a3ae8acf65631b9693a8532bd2968fe236d3
Alexis La Porte <aklaporte@gmail.com>
2016-09-21 17:17:07 -0400

    add accumulate

---
+++ javascript/accumulate/accumulate.js
@@ -0,0 +1,11 @@
+(function(){
+    "use strict";
+
+    function accumulate(array, callback) {
+        return array.map(function(e) {
+            return callback(e);
+        })
+    }
+
+    module.exports= accumulate;
+})();

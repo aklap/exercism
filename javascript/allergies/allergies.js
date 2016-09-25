7001add6dcc1053062a76ece9e62ecb8145654f2
Alexis La Porte <aklaporte@gmail.com>
2016-09-25 13:51:36 -0400

    add allergies solution

---
+++ javascript/allergies/allergies.js
@@ -0,0 +1,58 @@
+(function() {
+
+    "use strict";
+
+    function Allergies (tally) {
+        this.tally = tally;
+    }
+
+    Allergies.prototype.list = function () {
+        var tally = this.tally,
+            ALLERGIES = [
+                'eggs',
+                'peanuts',
+                'shellfish',
+                'strawberries',
+                'tomatoes',
+                'chocolate',
+                'pollen',
+                'cats'
+            ],
+
+            SCORES =[
+                1,
+                2,
+                4,
+                8,
+                16,
+                32,
+                64,
+                128
+            ],
+
+            remainder = this.tally;
+
+       return SCORES
+            .reverse()
+            .map(function(score, index) {
+                if (remainder % score < remainder) {
+                    remainder = remainder % score;
+                    return ALLERGIES[(SCORES.length-1) - index];
+                }
+            })
+           .reverse()
+           .filter(function(el) {
+                return el !== undefined;
+            });
+    };
+
+    Allergies.prototype.allergicTo = function (allergyToFind) {
+        var allergyList = this.list();
+
+        return allergyList.some(function(allergy) {
+            return allergy === allergyToFind;
+        });
+    }
+
+    module.exports = Allergies;
+})();
